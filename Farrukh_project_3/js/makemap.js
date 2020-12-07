473b5373d3b31b33d8ebb5984b83db51c2ad7970
mfarrukh752 <mfarrukh@worcester.edu>
2020-12-07 14:18:39 -0500

    Project 3 - Muhammad
    Project 3 - Muhammad

---
+++ Farrukh_project_3/js/makemap.js
@@ -0,0 +1,82 @@
+var map;
+
+function getColor(p) {
+		return p > .5 ? '#118900' :  //greater than 50%
+				   p > .4  ? '#17A803' : //greater than 40%
+				   p > .3  ? '#20DA06' : //greater than 30%
+				   p > .2  ? '#78FF00' : //greater than 20%
+							         '#FFEDA0'; //fall back value
+}
+
+//function to instantiate the Leaflet map
+function createMap(){
+
+    //create the map
+    map = L.map('mapid', {
+        center: [42.2626,-71.8023],
+        zoom: 11.5
+    });
+
+    //add  base tilelayer
+		var myBasemap =  L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',{
+				maxZoom: 17,
+        minZoom: 11.5,
+				attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+				});
+
+		myBasemap.addTo(map);
+
+    //call getData function
+    getData(map);
+
+    //create legend
+    var legend = L.control({position: 'bottomright'});
+        legend.onAdd = function (map) {
+
+            var div = L.DomUtil.create('div', 'info legend'),
+                grades = [20, 30, 40, 50],
+                labels = [];
+
+            // loop through our density intervals and generate a label with a colored square for each interval
+            for (var i = 0; i < grades.length; i++) {
+                div.innerHTML +=
+                    '<i style="background:' + getColor((grades[i]+1)/100) + '"></i> ' +
+                    grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+            }
+
+            return div;
+        };
+        legend.addTo(map);
+
+};
+
+
+
+//function to retrieve the data and place it on the map
+function getData(map){
+
+//because we are creating a choropleth map, we set the color with a function
+  function myStyle(feature) {
+    return {
+      weight: 3,
+      opacity: 1,
+      color: 'Blue',
+      dashArray: '3',
+      fillOpacity: 0.7,
+      //this equation uses the "getColor" function above to set the color based on the difference between votes divided by those cast
+      fillColor: getColor((feature.properties.biden-feature.properties.trump)/feature.properties.cast_total)
+    };
+  }
+
+  //load the data, then map
+    $.getJSON("data/worcElection20.geojson", function(response){
+
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(response, {
+									style: myStyle
+            }).addTo(map);
+    });
+};
+
+
+$(document).ready(createMap);
