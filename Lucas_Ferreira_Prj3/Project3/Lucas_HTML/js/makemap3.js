087bc955aea9958aee732f846d92bd2e74ae566d
Alex Tarr <avatarr8@gmail.com>
2020-12-17 09:54:35 -0500

    updates to Lucas' makemap for mutliple maps
    This updates Lucas' makemap.js and associated html/css so that multiple maps can be created using the same code.
    also includes additional arrays/variables for easier swapping of colors, breaks, and data.
    
    issues: the legend is still broken

---
+++ Lucas_Ferreira_Prj3/Project3/Lucas_HTML/js/makemap3.js
@@ -0,0 +1,108 @@
+var map;
+var geodata = "data/worcElection20.geojson"
+var mapdivs = ["biden","trump"];
+var breaks = [
+		[.5, '#03254c'],
+		[.4, '#1167b1'],
+		[.3, '#2a9df4'],
+		[.2, '#89cff0']
+];
+
+
+function getColor(p) {
+	fcolor = '#FFFFFF'; //set the feature color to white by default
+	for (index = 0; index < breaks.length; index++) { //go through however many breaks there are
+		if (p >= breaks[index][0]) { //if the result is above a break threshold, set the color and stop
+			fcolor = breaks[index][1];
+    	break;
+		}
+	}
+	return fcolor;
+}
+
+//because we are creating a choropleth map, we set the color with a function
+function myStyle(feature) {
+	//Here we set the equation to calculate what data each choropleth is showing
+	//the cases should match the mapdivs array above, which matches the IDs of divs in the HTML
+	//more math might be needed to get the output to fit in the breaks
+	//see for example Math.abs() here: https://www.w3schools.com/js/js_math.asp
+
+		switch (divcase) { //divcase is a variable set in the onEach function in the createMap function
+			case "biden":
+				result = (feature.properties.biden-feature.properties.trump)/feature.properties.cast_total
+				break;
+
+			case "trump":
+				result = (feature.properties.biden-feature.properties.trump)/feature.properties.cast_total
+				break;
+
+			default:
+				result = (feature.properties.biden-feature.properties.trump)/feature.properties.cast_total
+		}
+
+    return {
+      weight: 2,
+      opacity: 1,
+      color: 'silver',
+      dashArray: '5',
+      fillOpacity: .5,
+		  fillColor: getColor(result)
+			};
+  }//end of myStyle
+
+/*------------------------------------------------------------------------------------*/
+/*-------------- YOU SHOULD NOT HAVE TO EDIT CODE BELOW HERE to change the map--------*/
+/*------------------------------------------------------------------------------------*/
+
+//function to instantiate the Leaflet map
+function createMap(){
+	$.getJSON(geodata, function(response){
+
+		mapdivs.forEach((mapdiv, i) => { //adds a map for each div listed in mapdivs array above
+
+	    //create the map
+	    map = L.map(mapdiv, {
+	        center: [42.27,-71.8023],
+	        zoom:12
+	    });
+
+	    //add  base tilelayer
+			var myBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+					attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+					maxZoom: 16
+					});
+
+			myBasemap.addTo(map);
+
+			//create a Leaflet GeoJSON layer and add it to the map
+			divcase = mapdiv;
+			L.geoJson(response, {
+						style: myStyle
+			}).addTo(map);
+
+	    //call getData function
+	    //getData(map);
+
+	    //create legend
+	    var legend = L.control({position: 'bottomright'});
+	        legend.onAdd = function (map) {
+
+	            var div = L.DomUtil.create('div', 'info legend'),
+	                grades = [20, 30, 40, 50],
+	                labels = [];
+
+	            // loop through our density intervals and generate a label with a colored square for each interval
+	            for (var i = 0; i < grades.length; i++) {
+	                div.innerHTML +=
+	                    '<i style="background:' + getColor((grades[i]+1)/100) + '"></i> ' +
+	                    grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+	            }
+
+	            return div;
+	        };
+	        legend.addTo(map);
+		}); //end of mapdivs foreach
+	});//end of $.getJSON
+}; //end of createmap()
+
+$(document).ready(createMap);
